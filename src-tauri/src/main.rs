fdc3fcbc5968335753b5fb0f40e09175866b263c
breadthe <omigoshdev@protonmail.com>
2022-08-25 20:04:00 -0500

    FE <-> Rust comms

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,16 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![say_hi])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn say_hi(name: String) -> String {
+    println!("Hello {} from JS!🥳", name);
+
+    let output = "Hi back from Rust".to_string();
+
+    output
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,23 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  let rustResponse: string = "";
+
+  function sayHiToRust() {
+    invoke("say_hi", { name: "Rust" }).then(
+      (response) => (rustResponse = response)
+    );
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <button on:click={sayHiToRust}>Say hi to Rust</button>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {#if rustResponse}
+    <div>Rust response:</div>
+    <div>{rustResponse}</div>
+  {/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
